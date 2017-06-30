d2856a21bb7b4a06fd985c4f45d42f9d620183e0
kimouss <kim.quang.p@gmail.com>
2017-06-30 15:56:40 +0200

    remove AmountFormat

--- index.js
+++ index.js
@@ -33377,54 +33377,6 @@ module.exports = angular;
 
 },{"./angular":1}],3:[function(require,module,exports){
 require('angular')
-    .module('smartangels.filter.amount_format', [])
-    .filter('amountFormat', function(){
-        return function(input, decimals, extension, with_prefix) {
-
-            decimals = typeof decimals === 'number' ? decimals : 2;
-            extension = void 0 !== extension ? extension : '\u00A0€';
-            with_prefix = void 0 !== with_prefix ? with_prefix : false;
-            var minus = false;
-
-            if (typeof input === 'undefined') {
-                return input;
-            }
-
-            input = parseFloat(input);
-
-            if (input !== Math.abs(input)) {
-                minus = true;
-            }
-
-            var stringInput = input.toFixed(decimals),
-                indexOfDot = stringInput.indexOf('.'),
-                formatted = stringInput.replace(/\d(?=(?:\d{3})+(?:\.|$))/g, function($0, i){
-
-                    if (indexOfDot < 0 || i < indexOfDot) {
-                        return $0+'\u00A0';
-                    }
-
-                    return $0
-                });
-
-            formatted = formatted.replace('.', ',');
-
-            if (extension) {
-                formatted = formatted + extension;
-            }
-
-            if (minus) {
-                formatted = formatted.replace('-', '- ');
-            } else if (with_prefix) {
-                formatted = '+ ' + formatted;
-            }
-
-            return formatted;
-        }
-    });
-
-},{"angular":2}],4:[function(require,module,exports){
-require('angular')
     .module('smartangels.filter.phone_format', [])
     .filter('phoneFormat', function(){
         return function (tel) {
@@ -33445,4 +33397,4 @@ require('angular')
         };
     });
 
-},{"angular":2}]},{},[3,4]);
+},{"angular":2}]},{},[3]);
